e44a6c394bc1a8cc4687005ea1711c4d5996e67e
djavan93 <djavancosta93@gmail.com>
2023-10-15 14:20:40 -0300

    primeira aula

---
+++ src/main/java/br/com/djavan/todolist/user/UserModel.java
@@ -0,0 +1,11 @@
+package br.com.djavan.todolist.user;
+
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+
+    public String getName(){
+        return this.name;
+    }
+}
