fd1ba5346eb714b6a488692017448cf71c1c9f7d
djavan93 <djavancosta93@gmail.com>
2023-10-15 17:32:28 -0300

    Aula 2

--- src/main/java/br/com/djavan/todolist/user/UserController.java
+++ src/main/java/br/com/djavan/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.djavan.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getName());
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username já existe!");
+        }
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.ok(userCreated);
     }
 }
